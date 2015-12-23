a3cd7b55d7f387a3e0898a9dfe7e7d953f20e8cf
alebautt <id_bautista@hotmail.com>
2015-12-23 15:02:05 -0600

    Initial Commit

---
+++ PayPal Manual/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PayPal Manual
+//
+//  Created by Alejandra B on 23/12/15.
+//  Copyright © 2015 alebautista. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
